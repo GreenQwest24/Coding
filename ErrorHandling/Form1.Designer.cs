286d9ddfbc76729e7fc66fdd717b065e5e49eb9b
Laurence Green <lgreen24lg9@gmail.com>
2024-06-13 19:42:25 -0400

    Add project files.

---
+++ ErrorHandling/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ErrorHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label_ClickText = new Label();
+            textBox = new TextBox();
+            submitButton = new Button();
+            comboBox = new ComboBox();
+            clearButton = new Button();
+            checkBox = new CheckBox();
+            emptyText = new Button();
+            SuspendLayout();
+            // 
+            // label_ClickText
+            // 
+            label_ClickText.AutoSize = true;
+            label_ClickText.Location = new Point(308, 199);
+            label_ClickText.Name = "label_ClickText";
+            label_ClickText.Size = new Size(156, 25);
+            label_ClickText.TabIndex = 0;
+            label_ClickText.Text = "You Can Dub Click";
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(108, 117);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(150, 31);
+            textBox.TabIndex = 1;
+            // 
+            // submitButton
+            // 
+            submitButton.Location = new Point(108, 383);
+            submitButton.Name = "submitButton";
+            submitButton.Size = new Size(112, 34);
+            submitButton.TabIndex = 2;
+            submitButton.Text = "Click";
+            submitButton.UseVisualStyleBackColor = true;
+            submitButton.Click += submitButton_Click;
+            // 
+            // comboBox
+            // 
+            comboBox.FormattingEnabled = true;
+            comboBox.Location = new Point(499, 115);
+            comboBox.Name = "comboBox";
+            comboBox.Size = new Size(182, 33);
+            comboBox.TabIndex = 3;
+            // 
+            // clearButton
+            // 
+            clearButton.Location = new Point(569, 383);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(112, 34);
+            clearButton.TabIndex = 5;
+            clearButton.Text = "Clear";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
+            // checkBox
+            // 
+            checkBox.AutoSize = true;
+            checkBox.Location = new Point(323, 296);
+            checkBox.Name = "checkBox";
+            checkBox.Size = new Size(121, 29);
+            checkBox.TabIndex = 6;
+            checkBox.Text = "checkBox1";
+            checkBox.UseVisualStyleBackColor = true;
+            // 
+            // emptyText
+            // 
+            emptyText.Location = new Point(352, 383);
+            emptyText.Name = "emptyText";
+            emptyText.Size = new Size(112, 34);
+            emptyText.TabIndex = 7;
+            emptyText.Text = "Empty";
+            emptyText.UseVisualStyleBackColor = true;
+            emptyText.Click += emptyText_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 582);
+            Controls.Add(emptyText);
+            Controls.Add(checkBox);
+            Controls.Add(clearButton);
+            Controls.Add(comboBox);
+            Controls.Add(submitButton);
+            Controls.Add(textBox);
+            Controls.Add(label_ClickText);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label_ClickText;
+        private TextBox textBox;
+        private Button submitButton;
+        private ComboBox comboBox;
+        private Button clearButton;
+        private CheckBox checkBox;
+        private Button emptyText;
+    }
+}
---
+++ ErrorHandling/Form1.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ErrorHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        /* private void MainForm_Load(object sender, EventArgs e)
+         {
+             // Populate the combo box with some items
+             comboBox.Items.Add("Option 1");
+             comboBox.Items.Add("Option 2");
+             comboBox.Items.Add("Option 3");
+         }*/
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            if (checkBox.Checked)
+            {
+                label_ClickText.Text = "Apple";
+            }
+            else
+            {
+                MessageBox.Show("User needs to checkbox!");
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!checkBox.Checked)
+                {
+
+                    MessageBox.Show("CheckBox!");
+
+                }
+
+                else
+                {
+                    throw new Exception(" Test.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please enter some text in the TextBox.");
+                Console.WriteLine($"Invalid Input!", ex.Message);
+
+
+            }
+        }
+
+        private void emptyText_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox.Text == "")
+                {
+                    throw new Exception("Resubmit");
+
+                }
+                else
+                {
+                    textBox.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please enter some text in the TextBox.");
+                Console.WriteLine($"Invalid Input!", ex.Message);
+
+            }
+        }
+    }
+}
+
+
+
